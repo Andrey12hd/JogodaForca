e995b65f59e73dc735c2c3cfb1572ee8c492c914
Andrey12hd <168754313+Andrey12hd@users.noreply.github.com>
2025-04-23 11:43:48 -0300

    Add files via upload

---
+++ forca.py
@@ -0,0 +1,32 @@
+palavra = "ESCOLA"
+limite_tentativas = len(palavra) + 5
+acertou = False
+enforcou = False
+
+letras_acertadas = []
+for letra in palavra:
+    letras_acertadas.append("_")
+
+contador = 1
+while(not acertou and not enforcou):
+    print(letras_acertadas)
+    print("tentativas:",contador,"/",limite_tentativas)
+    chute = input("Digite e letra: ")
+
+    indice = 0
+
+    for letra in palavra:
+        if chute.upper() == letra:
+            letras_acertadas[indice] = chute.upper()
+        indice = indice +1
+    
+    if contador == limite_tentativas:
+        enforcou = True
+        print("Parabens voce errou seu incopetente :(\)")
+
+    if letras_acertadas.count("_") == 0:
+            acertou = True
+            print("Parabens, voce acetou a palavra")
+            print(letras_acertadas)
+
+    contador = contador + 1
